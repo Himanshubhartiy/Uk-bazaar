605ae4de1b836d7fbbea90c3f3fab1851eb96279
Himanshu Bhartiy <himanshubhartiy99@gmail.com>
2024-10-12 09:48:36 +0530

    Add WOW.js animations and configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './header';
-import Login from './login';
-import Signup from './Signup';
-import Home from './Home';
-import Card from './Card';
-import About from './About';
-import Categories from './Categories';
-import Carousel from './Carousel';
-import Footer from './Footer';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Login from "./Login";
+import Signup from "./Signup";
+import Home from "./Home";
+import Card from "./Card";
+import About from "./About";
+import Categories from "./Categories";
+import Carousel from "./Carousel";
+import Footer from "./Footer";
 
 function App() {
   return (
     <>
-<Router>
+      <Router>
         <Header />
         <Routes>
-        <Route 
-            path="/" 
+          <Route
+            path="/"
             element={
               <>
                 <Home />
                 <Card />
                 <About />
-                <Categories/>
+                <Categories />
                 <Carousel />
               </>
-            } 
+            }
           />
-          <Route path='/login'  element={<Login />}/>
-          <Route path='/signup' element={<Signup />}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
         <Footer />
       </Router>
